368279fcf88fe5177782d6b5a3a63a5efca903c7
Han <hangyulson1004@gmail.com>
2022-05-06 22:03:32 +0800

    Implement function detail

--- comp4901w/Final Project/BlindedMixer.sol
+++ comp4901w/Final Project/BlindedMixer.sol
@@ -1,40 +1,59 @@
+//Author: SON Hangyul
+
 pragma solidity >=0.7.0 <0.9.0;
 
-//Contract
 contract BlindedMixer {
-	mapping(address => bytes32) registeredHashes;
-	mapping(address => uint256) publicKeys;
-	mapping(uint256 => mapping(address => address)) publicKeyHashes;
-	mapping(address => address) lenderToUser;
-	mapping(address => address) UserToLender;
-
-//User
-	function deposit(bytes32 encodedHash) public payable{
-		//Must Declare Which public key to use
-	}
-
-	function withdraw(bytes32 signedHash, bytes32 originalHash, ) public {
-
-	}
-
-//Bank
-	function getUnsignedHash() public {
-		//
-	}
-
-	function sendSignedHash(bytes32 signedHash, address user) public {
-		//
-	}
-
-	 function registerAsBank(uint256 publicKey) public {
-		 //
-	 }
-
-	//Extra Credit User Can Call Withdraw without any deposit
-
-	//People
-	function lendToUser(address user) {
-
-	}
+	//Maps public key to 'submittedHashes' 
+	//Already have been initialized to a dynamic array
+	mapping(bytes => mapping(address => bytes32[])) publicKeyToHashes; 
+	mapping(bytes => uint256) publicKeyReferenceCount;
 	
+	mapping(bytes => address) publicKeyToBank;
+	mapping(address => bytes) bankToPublicKeys; //All public keys
+	mapping(address => uint256) bankDeposit; 
+	mapping(address => bytes32) userToSignedHash;
+	
+	
+	
+	uint256 depositDeadline;
+
+	constructor() {
+		//ASSUME 1 DAY 6400 BLOCK
+		depositDeadline = block.number + (6400 * 7);
+	}
+
+	fallback() external {}
+//User
+	function deposit(bytes32 encodedHash, address bank) public payable {
+		require(block.number < depositDeadline);
+		require(msg.value == 1 ether);
+		publicKeyToHashes[bankToPublicKeys[bank]][msg.sender].push(encodedHash);
+		publicKeyReferenceCount[bankToPublicKeys[bank]]++;
+	}
+
+	function getSignedHash() public returns (bytes32) {
+		return userToSignedHash[msg.sender];
+	}
+
+	function withdraw(address receiver, bytes12 nonce, bytes32 signedHash, uint8 v, bytes32 r, bytes32 s) public {
+		require(block.number > depositDeadline);
+		address bank = ecrecover(signedHash, v, r, s);
+		require(bank != address(0));
+
+		payable(msg.sender).transfer(0.01 ether);
+
+	}
+
+	function sendSignedHash(bytes32 signedHash, address user) public payable {
+		require(block.number < depositDeadline);
+		require(msg.value == 1 ether);
+		bankDeposit[msg.sender] += 1;
+		userToSignedHash[user] = signedHash;
+	}
+
+	//Decentralization
+	function registerAsBank(bytes memory publicKey) public {
+		 bankToPublicKeys[msg.sender] = publicKey;
+	 }
+	//Extra Credit User Can Call Withdraw without any deposit
 }
