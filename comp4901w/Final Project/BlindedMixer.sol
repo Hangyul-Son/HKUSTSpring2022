cce94a57251ae50a936e9731308388c5e756683e
Han <hangyulson1004@gmail.com>
2022-05-13 17:25:37 +0800

    Finalize BlindedMixer contract Implentation

--- comp4901w/Final Project/BlindedMixer.sol
+++ comp4901w/Final Project/BlindedMixer.sol
@@ -8,56 +8,69 @@ pragma solidity >=0.7.0 <0.9.0;
 //1. N uint128 (LARGE MULTIPLES OF TWO PRIME NUMBERS)
 //2. Public Keys uint256
 //3. Private Keys uint256
-//4. BASE uint120 (ENSURE BASE IS SMALLER THAN N)
+//4. BASE uint128 (ENSURE BASE IS SMALLER THAN N, BASE IS NOT DIVISIBLE BY N). 
 
 //4. EACH BANK IS ASSOCIATED WITH AN UNIQUE PUBLIC KEY
 //5. EACH USER ADDRESS CAN SUBMIT ONE SIGNED MESSAGE
+//6. User submitted encoded messages can not be  '0'
 
 contract BlindedMixer {
 	mapping(uint256 => mapping(address => uint128)) publicKeyToEncodedMessages; 
+	mapping(uint256 => mapping(uint128 => address)) publicKeyToAddress;
 	mapping(uint256 => address) publicKeyToBank;
-	mapping(uint256 => uint256) publicKeyToDeadline;
-	mapping(uint256 => uint256) publicKeyReferenceCount;
+	mapping(uint256 => uint128) publicKeyToN;
+	mapping(uint256 => uint256) publicKeyToDepositDeadline;
+	mapping(uint256 => uint256) publicKeyToWithdrawDeadline;
 
-	mapping(address => uint128) bankToN;
+	mapping(uint256 => uint256) publicKeyReferenceCount;
+	mapping(uint256 => uint256) publicKeyWithdrawCount;
+
 	mapping(address => uint256) bankToPublicKey; 
 	mapping(address => uint256) bankDeposit; 
 
 	mapping(address => uint128) userToSignedMessage;
 	mapping(address => bool) userMessageSigned;
-	mapping(bytes32 => bool) signedHashUsed;
+	mapping(uint128 => bool) signedMessageUsed;
 
 	constructor() {}
 	fallback() external {}
 
-	//USER
+	//---------------------------------------USER-----------------------------------------------
 	function deposit(uint128 encodedMessage, uint256 publicKey) public payable {
-		require(publicKeyToDeadline[publicKey] > block.number);
+		require(publicKeyToDepositDeadline[publicKey] > block.number);
+		require(encodedMessage != 0); 
 		require(msg.value == 1 ether);
 
 		publicKeyToEncodedMessages[publicKey][msg.sender] = encodedMessage;
+		publicKeyToAddress[publicKey][encodedMessage] = msg.sender;
 		publicKeyReferenceCount[publicKey] += 1;
 	}
 
-	function refundDeposit(uint256 publicKey) public {
-		require(publicKeyToDeadline[publicKey] <= block.number);
+	function refundUserDeposit(uint128 encodedMessage, uint256 publicKey) public {
+		require(publicKeyToDepositDeadline[publicKey] <= block.number);
+		require(publicKeyToAddress[publicKey][encodedMessage] == msg.sender);
 		require(!userMessageSigned[msg.sender]);
 
 		userMessageSigned[msg.sender] = true;
 		payable(msg.sender).call{value: 1 ether, gas:2300}("");
 	}
 
-	function withdraw(address receiver, bytes12 nonce, bytes32 signedHash, address bank) public {
-		require(publicKeyToDeadline[bankToPublicKey[bank]] <= block.number);
-		require(keccak256(abi.encodePacked(receiver, nonce)) == signedHash);
-		require(checkSignedHash(signedHash, bank) == true);
-		require(signedHashUsed[signedHash] == false);	
+	function withdraw(address receiver, uint64 nonce, uint128 signedMessage, address bank) public {
+		uint256 publicKey = bankToPublicKey[bank];
+		require(publicKeyToDepositDeadline[publicKey] <= block.number);
+		require(publicKeyToWithdrawDeadline[publicKey] > block.number);
 
-		signedHashUsed[signedHash] = true;
-		payable(bank).call{value: 1.01 ether, gas:2300}("");
+		bytes32 originalHash = keccak256(abi.encodePacked(receiver, nonce));
+		require(validateSignedMessage(originalHash, signedMessage, bank));
+		require(!signedMessageUsed[signedMessage]);	
+
+		publicKeyWithdrawCount[publicKey] += 1;
+		signedMessageUsed[signedMessage] = true;
 		
+		payable(bank).call{value: 0.01 ether, gas:2300}("");
+
 		if(msg.sender != receiver){
-			payable(receiver).call{value: 0.98 ether, gas:2300}(""); //
+			payable(receiver).call{value: 0.98 ether, gas:2300}("");
 			payable(msg.sender).call{value: 0.01 ether, gas:2300}("");
 		}
 		else {
@@ -65,36 +78,51 @@ contract BlindedMixer {
 		}
 	}
 
-
-	//BANK
-	function sendSignedMessage(uint128 signedMessage, address user) public payable {
-		uint256 publicKey = bankToPublicKey[msg.sender];
-		require(block.number < publicKeyToDeadline[publicKey]);
+	//---------------------------------------BANK-----------------------------------------------
+	function submitSignedMessage(uint128 signedMessage, address user) public payable {
+		uint256 publicKey = bankToPublicKey[msg.sender];	
+		require(publicKeyToDepositDeadline[publicKey] > block.number);
 		require(msg.value == 1 ether);
-		require(fastExponentiation(signedMessage, publicKey, bankToN[msg.sender]) == publicKeyToEncodedMessages[publicKey][user]); 
+	
+		uint128 encodedMessage = uint128(fastExponentiation(signedMessage, publicKey, publicKeyToN[publicKey]));
+		require(publicKeyToAddress[publicKey][encodedMessage] == user);
+		require(!userMessageSigned[user]);
 
+		bankDeposit[msg.sender] += msg.value;
 		userToSignedMessage[user] = signedMessage;
 		userMessageSigned[user] = true;
 	}
 
+	function refundBankDeposit() public {
+		uint256 publicKey = bankToPublicKey[msg.sender];
+		require(publicKeyToWithdrawDeadline[publicKey] <= block.number);
+		require(publicKeyReferenceCount[publicKey] >= publicKeyWithdrawCount[publicKey]);
+		
+	  uint256 deposit = bankDeposit[msg.sender]; 
+		bankDeposit[msg.sender] = 0;
+		payable(msg.sender).call{value: deposit, gas:2300}("");
+	}
+
 	function registerAsBank(uint256 publicKey, uint128 N) public {
-		require(publicKeyToDeadline[publicKey] == 0);
+		require(publicKey != 0);
+		require(publicKeyToDepositDeadline[publicKey] == 0);
+		require(bankToPublicKey[msg.sender] == 0);
 
 		bankToPublicKey[msg.sender] = publicKey;
-		bankToN[msg.sender] = N;
+		publicKeyToN[publicKey] = N;
 
 		publicKeyToBank[publicKey] = msg.sender;
-		publicKeyToDeadline[publicKey] = block.number + (6400 * 7);
+		publicKeyToDepositDeadline[publicKey] = block.number + (6400 * 7);
+		publicKeyToWithdrawDeadline[publicKey] = block.number + (6400 * 14);
 	 }
 
-	//UTILS
-	function checkSignedHash(bytes32 signedHash, address bank) view internal returns (bool) {
-		uint128 N = bankToN[bank];
+	//---------------------------------------UTILS-----------------------------------------------
+	function validateSignedMessage(bytes32 originalHash, uint128 signedMessage, address bank) view internal returns (bool) {
 		uint256 publicKey = bankToPublicKey[bank];
-		uint120 base = uint120(uint256(signedHash));  //WHY NOT UINT128? PREVENT BASE FROM BEING MULTIPLES OF N
-
-		uint120 result = uint120(fastExponentiation(base, publicKey, N));
-		return result == base;
+		uint128 N = publicKeyToN[publicKey];
+		uint128 originalMessage = uint128(uint256(originalHash));  
+		uint128 result = uint128(fastExponentiation(signedMessage, publicKey, N)); //4. SHOULD PREVENT BASE FROM BEING LARGER THAN N
+		return result == originalMessage;
 	}
 
 	function fastExponentiation(uint128 base, uint256 exponent, uint128 N) internal pure returns (uint256) {
@@ -111,5 +139,10 @@ contract BlindedMixer {
 			}
 		}
 		return result;
+	}
+
+	//---------------------------------------DUBUG-UTILS--------------------------------------------
+	function getMessage(address receiver, uint128 nonce) view public returns (uint128) {
+		return uint128(uint256(keccak256(abi.encodePacked(receiver, nonce))));
 	}
 }
